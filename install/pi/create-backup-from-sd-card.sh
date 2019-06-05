411308ea799c14dd3f0b95d3219cbbdab7deb401
jay-johnson <jay.p.h.johnson@gmail.com>
2019-06-04 20:48:34 -0700

    create backup from sd card support

--- install/pi/create-backup-from-sd-card.sh
+++ install/pi/create-backup-from-sd-card.sh
@@ -8,20 +8,38 @@ if [[ -e ${DCPATH}/files/bash_colors.sh ]]; then
 fi
 
 # latest donkey car image id:
-latest_image="donkey_2.6.1_pi3.img"
+latest_version="donkey_2.6.1_pi3"
+latest_image="${latest_version}.img"
+latest_zip="${latest_version}.zip"
 device="/dev/sdf"
 if [[ "${DEVICE}" != "" ]]; then
     device="${DEVICE}"
 fi
-backup_dir="${DCPATH}/backups"
+backup_dir="${DCPATH}/backups/img"
 if [[ "${DCBACKUPDIR}" != "" ]]; then
     backup_dir="${DCBACKUPDIR}"
+fi
+zip_dir="${DCPATH}/backups/zip"
+if [[ "${DCBACKUPDIR}" != "" ]]; then
+    zip_dir="${DCBACKUPDIR}"
 fi
 
 if [[ ! -e ${backup_dir} ]]; then
     mkdir -p -m 777 ${backup_dir}
 fi
+if [[ ! -e ${zip_dir} ]]; then
+    mkdir -p -m 777 ${zip_dir}
+fi
 backup_file="${backup_dir}/${latest_image}"
+zip_file="${zip_dir}/${latest_zip}"
+
+if [[ -e ${backup_file} ]] || [[ -e ${zip_file} ]]; then
+    latest_version="donkey_2.6.1_pi3_$(date +'%Y_%m_%d_%H_%M_%S')"
+    latest_image="${latest_version}.img"
+    latest_zip="${latest_version}.zip"
+    backup_file="${backup_dir}/${latest_image}"
+    zip_file="${zip_dir}/${latest_zip}"
+fi
 
 anmt "creating ${device} backup to: ${backup_file}"
 dd if=${device} of=${backup_file} bs=512 status=progress
@@ -29,4 +47,21 @@ if [[ "$?" != "0" ]]; then
     err "failed creating ${device} backup to: ${backup_file}"
     exit 1
 fi
+
+if [[ ! -e ${backup_file} ]]; then
+    err "failed to find backup file for ${device} at: ${backup_file}"
+    exit 1
+fi
+anmt "creating image zip: ${backup_file} as ${zip_file}"
+zip ${zip_file} ${backup_file}
+
+if [[ ! -e ${zip_file} ]]; then
+    err "failed to find zip file for ${device} at: ${zip_file}"
+    exit 1
+fi
+
+anmt "${latest_version} - backups completed"
+anmt "image: ${backup_file}"
+anmt "zip: ${zip_file}"
+
 exit 0
